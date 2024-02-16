f1bc9ab37747624ce125f68133c4458749447454
3MM4NU3L98 <emmanuelsegeard@gmail.com>
2024-02-16 04:32:37 +0100

    mentorat 16-02-2024

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,11 +1,31 @@
-import { host, galeriePortefolio } from "./module/const.js";
-import { afficheGalerie } from "./module/afficeGalerie.js";
+import { host, galeriePortefolio, filtresPortefolio } from "./module/const.js";
+import { afficheFiltres } from "./module/afficheFiltres.js";
+import { afficheGalerie } from "./module/afficheGalerie.js";
 
 console.log(`${host}/works`)
 // Récupération des travaux depuis l'API en JSON
 const travaux = await fetch(`${host}/works`).then(travaux => travaux.json());
+// Récupération des catégories de travaux depuis l'API en JSON
+const categoriesTravaux = await fetch(`${host}/categories`).then(categoriesTravaux => categoriesTravaux.json());
+
 
 console.log(travaux)
+console.log(categoriesTravaux)
+
+let travauxAfficher = travaux;
+
+let choixCategories = afficheFiltres(categoriesTravaux, filtresPortefolio);
+console.log(choixCategories)
+
+if (choixCategories !== 0) {
+    //  travauxAfficher flitré par choixCatégorie
+} else { }
 
 
-afficheGalerie(travaux, galeriePortefolio)
+afficheGalerie(travauxAfficher, galeriePortefolio);
+
+
+
+
+
+
--- FrontEnd/assets/js/module/const.js
+++ FrontEnd/assets/js/module/const.js
@@ -4,4 +4,7 @@ export const host = "http://localhost:5678/api";
 // Récupération de l'élément du DOM qui accueillera la galerie
 export const galeriePortefolio = document.querySelector(".gallery");
 
+// Récupération de l'élément du DOM qui accueillera le choix du filtre
+export const filtresPortefolio = document.getElementById("filtres")
+
 
